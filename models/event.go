a98799f8fac6ded9bb94a579ce0c50c0c08ab7a2
Oduyale Enoch <enoch.oduyale@reventtechnologies.com>
2026-03-04 15:25:26 +0100

    feat: events management app

--- models/event.go
+++ models/event.go
@@ -64,3 +64,40 @@ func GetAllEvents() ([]Event, error) {
 
 	return events, nil
 }
+
+func (event Event) Update() error {
+	query := `
+	
+	UPDATE events
+	SET name = ?, description = ?, location = ?, dateTime = ?
+	WHERE id = ?
+	`
+
+	stmt, err := db.DB.Prepare(query)
+
+	if err != nil {
+		return err
+	}
+
+	defer stmt.Close()
+
+	_, err = stmt.Exec(event.Name, event.Description, event.Location, event.DateTime, event.ID)
+	return err
+}
+
+func GetEventById(id int64) (*Event, error) {
+
+	query := "SELECT * FROM events WHERE id = ?"
+
+	row := db.DB.QueryRow(query, id)
+
+	var event Event
+	err := row.Scan(&event.ID, &event.Name, &event.Description, &event.Location, &event.DateTime, &event.DateTime, &event.UserID)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return &event, nil
+
+}
